940466a0e7c6b2558894385c5ee3b26fd585c5cb
Stas Zelenko <sharcik14@gmail.com>
2016-05-15 14:48:43 +0300

    fix error with registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    
     if @user.save
-      sign_in @user
+      login @user
       redirect_to questions_path
     else
       render 'new'
