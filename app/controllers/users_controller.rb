3951d00516617466c6679f5a106637bf9245b535
Stas Zelenko <sharcik14@gmail.com>
2016-05-24 09:55:36 +0300

    deploy heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)   
 
     if @user.save
-      create_tag(params[:tags])
       login @user
+      create_tag(params[:tags])
       redirect_to questions_path
     else
       render 'new'
